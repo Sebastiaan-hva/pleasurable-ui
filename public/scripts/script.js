a6d239758b4d79b1f9282ba1a0e12bfcff25b515
Luuk <147055722+Lutrian1@users.noreply.github.com>
2025-05-24 00:12:19 +0200

    #83: Werkt al beter
    Toegevoegd:
    
    - Rotate met math.Random(), die de var (--random-degrees) aanpast binnen css. Waardoor je dus constant een willekeurige kant krijgt met hoe de bloem draait.
    
    - Toegevoegd dat de header weggaat, alleen de gift kaarten gaan nog niet weg.
    
    - Fixed aan de DOM dat ervoor zorgt dat je niet kan scrollen

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,7 +1,13 @@
+let max = 180
+let min = -180
+let animationDuration = 1400
+
 let createListCard = document.querySelector('.create-list-card');
 let flower = document.querySelector('.flower'); 
+let DOM = document.documentElement;
+let header = document.querySelector('header');
 let giftBoxes = document.querySelectorAll('.gift-box'); // Dit selecteerd alle gift-boxes, Bron: https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelectorAll
-
+let randomNumber = null; // https://css-tricks.com/random-numbers-css/: Is nu niks maar wordt in de listAni aangepast tot een random number.
 
 giftBoxes.forEach(giftBox => {
     giftBox.addEventListener('click', giftBoxAni,);
@@ -10,14 +16,50 @@ giftBoxes.forEach(giftBox => {
 createListCard.addEventListener('click', listAni,);
 
 function listAni() {
+
     createListCard.classList.add('clicked');
+
         setTimeout(() => {
-            console.log("Korte Timeout voor animatie");
+
+            console.log("Animatie begint");
             createListCard.classList.remove('clicked');
+
+            // Zoekt een willekeurig nummer tussen 180 en -180 als het maar niet tussen 45 en -45 zit. Zowel, dan probeert de loop opnieuw.
+            // Mijn test: https://codepen.io/Lutrian1/pen/yyNBEjj
+
+            do{
+                randomNumber = Math.random() * (max - min) + min;
+                console.log("Random Number =", randomNumber);     
+            } while (randomNumber >= -25 && randomNumber <= 25);
+      
+            console.log("Juiste nummer gevonden:", randomNumber);
+
+            flower.style.setProperty('--random-degrees', randomNumber + 'deg');
             flower.classList.add('flowerAni'); 
-        }, 250);
-        flower.classList.remove('flowerAni'); 
+
+            //Z-Index aanpassen en ervoor zorgen dat de DOM niet scrollbaar is
+            flower.style.zIndex = '1000';
+            header.style.zIndex = '-1000'
+            
+            setTimeout(() => {
+                DOM.style.position  = 'fixed';
+            }, 350);
+            
+
+        }, 250); // setTimeout is de duur van de 'click animatie'
+
+        setTimeout(() => {
+            flower.classList.remove('flowerAni');
+            //Verwijderd weer alles na de duur van de animatie
+            DOM.style.position = ''
+            flower.style.zIndex = '';
+            header.style.zIndex = ''
+
+        }, (animationDuration)); // Duur animatie
+    
 }
+
+// Ik maak hierboven een loop dat de var in mijn stylesheet 'random-degrees' dynamisch aanpast na het clicken op de 'createListCard'. Dit getal word willekeurig aangepast
 
 function giftBoxAni(event) { // Gebruik van event dat niet uitmaakt waarop de .clicked word toegevoegd. Bron: https://learnjavascript.online (In deze course staat, dus volg deze course weer om dit te begrijpen)
     event.target.classList.add('clicked');
